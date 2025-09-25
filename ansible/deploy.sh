8f21bbcc711b7018e41d404e2ed80bb662cd3eec
corentinpn <corentinp927@gmail.com>
2025-09-25 14:30:43 +0200

    test pipeline

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -3,17 +3,12 @@
 # Aller dans le dossier Terraform
 cd ../infra
 
-echo " Initialisation de Terraform..."
+# Lancer Terraform
 terraform init
-
-echo " Application de l'infrastructure..."
 terraform apply -auto-approve
 
 # Revenir dans le dossier Ansible
 cd ../ansible
 
-echo " Provisionnement avec Ansible..."
+# Lancer Ansible
 ansible-playbook provision.yml -i inventory.ini
-
-echo " Déploiement avec Ansible..."
-ansible-playbook deploy.yml -i inventory.ini
